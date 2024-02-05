a3dd92415d74de83118002e3c480d859b819b57c
hcmannin <147657511+hcmannin@users.noreply.github.com>
2024-02-04 17:45:33 -0800

    Delete educollage-main directory

--- educollage-main/script.js
+++
@@ -1,6 +0,0 @@
-const wrapper = document.querySelector(".search-sec"),
-selectBtn = wrapper.querySelector(".select-btn");
-
-selectBtn.addEventListener("click", () => {
-    wrapper.classList.toggle("active");
-});
